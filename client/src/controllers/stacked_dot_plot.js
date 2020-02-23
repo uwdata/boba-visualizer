uwdata/boba-visualizer

commit 3c37ed752bf318d8e3df334ba917a66874fcc585

Modify density dot algorithm such that data points with different signs are separated

client/src/controllers/stacked_dot_plot.js
@@ -306,6 +306,41 @@ class StackedDotPlot {
   }
 
   /**
+   * Density dot algorithm, assuming data is sorted.
+   * @param start
+   * @param end
+   * @param forward Whether start should be smaller than end.
+   * @private
+   */
+  _computeDensityDots (start, end, forward) {
+    let scale = this.scale
+    let data = this.data
+    let bin_size = this.dot_radius  // x-axis half bin size
+
+    // dot density algorithm
+    // assuming data is sorted
+    let x = null
+    let count = 0
+    let i = start
+    while (forward ? i < end : i > end) {
+      let xi = scale.x(data[i].diff)
+      let within = forward ? (xi < x + bin_size && xi >= x - bin_size) :
+        (xi <= x + bin_size && xi > x - bin_size)
+      if (x != null && within) {
+        count += 1
+      } else {
+        x = forward ? xi + bin_size : xi - bin_size
+        count = 0
+      }
+
+      data[i]._x = x
+      data[i]._y = count
+
+      i += forward ? 1 : -1
+    }
+  }
+
+  /**
    * Draw density dot plots (from Allison & Cicchetti, 1976) without smoothing
    * Opacity will be adjusted based on the amount of overlap
    * @param parent
@@ -316,27 +351,11 @@ class StackedDotPlot {
   _drawDensityDots (parent, redraw = false) {
     let scale = this.scale
     let data = this.data
-    let bin_size = this.dot_radius * 2  // x-axis bin size
 
-    // dot density algorithm
-    // assuming data is sorted
-    let i = 0
-    let x = null
-    let count = 0
-    while (i < data.length) {
-      let xi = data[i].diff
-      if (x != null && scale.x(xi) < scale.x(x) + bin_size) {
-        count += 1
-      } else {
-        x = xi
-        count = 0
-      }
-
-      data[i]._x = x
-      data[i]._y = count
-
-      i += 1
-    }
+    const sign = 0
+    let i = _.findIndex(data, (d) => d.diff >= sign)
+    this._computeDensityDots(i, data.length, true)
+    this._computeDensityDots(i - 1, -1, false)
 
     // compute y based on counts
     let dm = scale.x.domain()
@@ -354,13 +373,13 @@ class StackedDotPlot {
         .append('circle')
         .classed('dot', true)
         .attr('r', () => this.dot_radius)
-        .attr('cx', (d) => scale.x(d._x))
+        .attr('cx', (d) => d._x)
         .attr('cy', (d) => d._y)
         .attr('fill-opacity', opacity)
     } else {
       dots.transition()
         .duration(1000)
-        .attr('cx', (d) => scale.x(d._x))
+        .attr('cx', (d) => d._x)
         .attr('cy', (d) => d._y)
         .attr('fill-opacity', opacity)
     }

client/src/controllers/vis/brushX.js
@@ -22,7 +22,7 @@ class BrushX {
 
       // x0, x1
       let sel = _.flatten(d3.event.selection)
-      let bounds = _.map(sel, (s) => scale.x.invert(s))
+      let bounds = _.map(sel, (s) => s)
 
       // change color of selected points
       d3.selectAll(selector)
@@ -41,7 +41,7 @@ class BrushX {
 
       // x0, x1
       let sel = _.flatten(d3.event.selection)
-      let bounds = _.map(sel, (s) => scale.x.invert(s))
+      let bounds = _.map(sel, (s) => s)
 
       let pts = _.filter(data, (p) => {
         return scale.getRawX(p) >= bounds[0] &&

commit d9a75cf2b399ee7f224b0a3916b4bacd8f4b0072

Fix bug introduced by [3c37ed7]

client/src/controllers/stacked_dot_plot.js
@@ -373,7 +373,7 @@ class StackedDotPlot {
     this._computeDensityDots(i - 1, -1, false)
 
     // compute y based on counts
-    let dm = scale.x.domain()
+    let dm = scale.x.range()
     let maxy = d3.max(data, (d) => d._x >= dm[0] && d._x <= dm[1] ? d._y : 0)
     let step = Math.min(scale.height() / (maxy + 1), this.dot_radius * 2)
     _.each(data, (d) => {